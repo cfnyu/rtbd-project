8de3d4a5eb7201182d144ea41bf5fec1995f8c8a
Radhika Mattoo <rm3485@nyu.edu>
2018-07-31 16:42:41 -0400

    mapper code ignores score in weight calculation, updated readme

--- step-4-process-topics/README.md
+++ step-4-process-topics/README.md
@@ -1,5 +1,31 @@
+## Grouping Hive Data
+
+### Overview
+The final step of the project is associating 'topics' found from the word count and associating them with Stack Overflow questions via a weighted search.
+
+Since we need to iterate over 2 datasets at once, Hadoop's [Distributed Cache](https://hadoop.apache.org/docs/r2.6.3/api/org/apache/hadoop/filecache/DistributedCache.html) was used to hold the topic word count.
+
+Inside the map code, we take a single Stack Overflow question and calculate its weight for every topic. The weights are valued as such:
+
+```
+Topic in question body: 0.3
+Topic in question title: 0.3
+Topic equals one of the question's tags: 0.4
+```
+Since we want to maintain our date grouping, we must keep the date as the key. However, to make our lives easier, we attach the topic name to the key so the reducer can sort questions (grouped by date and topic) by their given weights.
+
+In the reducer, we split the topic from the date in the key, and sort the corresponding question list by their weights. We then output  `date:topic:qId1,qId2,....qIdn` to HDFS for a file that contains all the data we need, but is still small enough to download and use in our UI.
+
+
+### Build
 ```
 mvn compile
 mvn package
-hadoop jar analytic-1.0-SNAPSHOT-jar-with-dependencies.jar posts-clean/part-r-00000 test hdfs:///user/rm3485/county.csv
+```
+
+### Run
+Given an input HDFS directory of `posts-clean` and the output file from step 3 called `topic_data`:
+
+```
+hadoop jar target/analytic-1.0-SNAPSHOT-jar-with-dependencies.jar posts-clean finalData topic_data
 ```
--- step-4-process-topics/src/main/java/rm3485/AnalyticMapper.java
+++ step-4-process-topics/src/main/java/rm3485/AnalyticMapper.java
@@ -12,14 +12,17 @@ import java.lang.StringBuilder;
 
 public class AnalyticMapper extends Mapper<LongWritable, Text, Text, Text> {
   private ArrayList<String> topics = new ArrayList<String>();
-  private double topicEqualsTag = 0.3;
-  private double topicInBody = 0.2;
-  private double topicInTitle = 0.2;
+  private double topicEqualsTag = 0.4;
+  private double topicInBody = 0.3;
+  private double topicInTitle = 0.3;
 
+  // private double topicEqualsTag = 0.3;
+  // private double topicInBody = 0.2;
+  // private double topicInTitle = 0.2;
   // Score range optimal in 7 - 20
-  private double scoreInRange = 0.3;
-  private double scoreBelowRange = 0.1;
-  private double scoreAboveRange = 0.2;
+  // private double scoreInRange = 0.3;
+  // private double scoreBelowRange = 0.1;
+  // private double scoreAboveRange = 0.2;
 
   @Override
   protected void setup(Context context) throws IOException,InterruptedException{
@@ -41,79 +44,81 @@ public class AnalyticMapper extends Mapper<LongWritable, Text, Text, Text> {
 
   @Override
   public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-    // Get StackOverflow line data
     try{
       String line = value.toString();
       String[] split = line.split(",");
       if(split.length >= 5){
-         String questionId = split[0];
-         // TODO: Filter questions by score?
-         int score = Integer.parseInt(split[1]);
+        String questionId = split[0];
+        // TODO: Filter questions by score?
+        int score = Integer.parseInt(split[1]);
 
-          if(score > 8){
+        if(score > 8){
 
-             String title = split[2];
-             String[] tagList = split[3].trim().split(" ");
+           String title = split[2];
+           String[] tagList = split[3].trim().split(" ");
 
-      // Rebuild body after "," split
-             StringBuilder bodyBuilder = new StringBuilder();
-             if(split.length > 5){
-               for(int i = 4; i < split.length; i++){
-                 bodyBuilder.append(split[i]);
-               }
-             }else{
-               bodyBuilder.append(split[4]);
+           // Rebuild body after "," split
+           StringBuilder bodyBuilder = new StringBuilder();
+           if(split.length > 5){
+             for(int i = 4; i < split.length; i++){
+               bodyBuilder.append(split[i]);
              }
-             String body = bodyBuilder.toString();
+           }else{
+             bodyBuilder.append(split[4]);
+           }
+           String body = bodyBuilder.toString();
 
-             // Split tags by space and add to arraylist
-             ArrayList<String> tags = new ArrayList<String>();
-             if(tagList.length > 1){ // several tags
-               for(String itm : tagList){
-                 tags.add(itm);
-               }
-             }else{ //only one tag
-               tags.add(tagList[0]);
+           // Split tags by space and add to arraylist
+           ArrayList<String> tags = new ArrayList<String>();
+           if(tagList.length > 1){ // several tags
+             for(String itm : tagList){
+               tags.add(itm);
              }
+           }else{ //only one tag
+             tags.add(tagList[0]);
+           }
 
-      // For each topic, generate a weight for the SO question
-      Iterator<String> it = topics.iterator();
-      while(it.hasNext()){
-        String[] data = it.next().split(":"); //date:topic
-        String date = data[0].trim();
-        String topic = data[1].trim();
-        String count = data[2].trim();
-        double weight = 0.0;
+          // For each topic, generate a weight for the SO question
+          Iterator<String> it = topics.iterator();
+          while(it.hasNext()){
+            String[] data = it.next().split(":"); //date:topic
+            String date = data[0].trim().toLowerCase();
+            String topic = data[1].trim().toLowerCase();
+            String count = data[2].trim();
+            double weight = 0.0;
 
-        if(body.contains(topic)){
-          weight += topicInBody;
-        }if(title.contains(topic)){
-          weight += topicInTitle;
-        }
-        Iterator<String> tags_it = tags.iterator();
-        while(tags_it.hasNext()){
-          String tag = tags_it.next();
-          if(topic.equals(tag)){
-            weight += topicEqualsTag;
+
+            // FIXME: Look for whole words
+            String spaced_topic = " " + topic + " ";
+            if(body.contains(spaced_topic)){
+              weight += topicInBody;
+            }if(title.contains(spaced_topic)){
+              weight += topicInTitle;
+            }
+            Iterator<String> tags_it = tags.iterator();
+            while(tags_it.hasNext()){
+              String tag = tags_it.next();
+              if(topic.equals(tag)){
+                weight += topicEqualsTag;
+              }
+            }
+            // if(score >= 7 && score <= 20){
+            //   weight += scoreInRange;
+            // }else if(score < 7){
+            //   weight += scoreBelowRange;
+            // }else{
+            //   weight += scoreAboveRange;
+            // }
+
+            String weightString = Double.toString(weight);
+            if(weight > 0.0 && weightString.length() > 0  && questionId.length() > 0){
+              String output_value =  weightString + ":" + questionId;
+              String output_key = date + ":" + topic;
+              context.write(new Text(output_key), new Text(output_value));
+            }
           }
-        }
-        if(score >= 7 && score <= 20){
-          weight += scoreInRange;
-        }else if(score < 7){
-          weight += scoreBelowRange;
-        }else{
-          weight += scoreAboveRange;
-        }
-
-        String weightString = Double.toString(weight);
-        if(weight > 0.0 && weightString.length() > 0  && questionId.length() > 0){
-          String output_value =  weightString + ":" + questionId;
-          String output_key = date + ":" + topic;
-          context.write(new Text(output_key), new Text(output_value));
-        }
       }
     }
-  }
   }catch(Exception e){
       System.out.println(e.getMessage());
     }
