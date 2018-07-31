92dedae919c1fcbf988d7d12f56a7e2ebccb35c7
Radhika Mattoo <rm3485@nyu.edu>
2018-07-31 01:50:15 -0400

    fixed if statement wrapping

--- step-4-process-topics/src/main/java/rm3485/AnalyticReducer.java
+++ step-4-process-topics/src/main/java/rm3485/AnalyticReducer.java
@@ -16,26 +16,33 @@ public class AnalyticReducer extends Reducer<Text, Text, Text, NullWritable> {
   public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException{
     String data_string = key.toString();
     String[] dataArr = data_string.split(":");
-    String date = dataArr[0];
-    String topic = dataArr[1];
-    int max = 50;
-    // Date:Topic, ["weight:id", "weight:id"]
-    ArrayList<String> data = new ArrayList<String>();
-    for(Text value: values){
-      data.add(value.toString().trim());
-    }
-    // Sort by weight
-    Collections.sort(data, (o1, o2) -> Double.compare(Double.parseDouble(o1.split(":")[0]), Double.parseDouble(o2.split(":")[0]) ) );
-    Collections.reverse(data);
+    if(dataArr.length == 2){
+      String date = dataArr[0];
+      String topic = dataArr[1];
+      int max = 50;
+      // Date:Topic, ["weight:id", "weight:id"]
+      ArrayList<String> data = new ArrayList<String>();
+      for(Text value: values){
+        data.add(value.toString().trim());
+      }
+      // Sort by weight
+      Collections.sort(data, (o1, o2) -> Double.compare(Double.parseDouble(o1.split(":")[0]), Double.parseDouble(o2.split(":")[0]) ) );
+      Collections.reverse(data);
 
-    int size = data.size() > max ? max : data.size();
-    StringBuilder sb = new StringBuilder(date + ":" + topic + ":");
-    for(int i = 0; i < size; i++){
-      String id = data.get(i).split(":")[1];
-      sb.append(id + ",");
+      int size = data.size() > max ? max : data.size();
+      StringBuilder sb = new StringBuilder(date + ":" + topic + ":");
+      for(int i = 0; i < size; i++){
+        String id = data.get(i).split(":")[1];
+        sb.append(id + ",");
+      }
+      String output = sb.toString();
+      String final_output = output.substring(0, output.length() - 1);
+      context.write(new Text(final_output), NullWritable.get());
+    }else{
+      for(Text t : values){
+        context.write(t, NullWritable.get());
+      }
     }
-    String output = sb.toString();
-    String final_output = output.substring(0, output.length() - 1);
-    context.write(new Text(final_output), NullWritable.get());
+
   }
 }
