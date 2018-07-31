78ca6ad318c9edea299ac4e2a7bc09f6d2001c21
Christian Fernandez <cfnyu23@gmail.com>
2018-07-30 23:35:11 -0400

    Update run script for WordCount

--- step-3-word-count/run.sh
+++ step-3-word-count/run.sh
@@ -2,11 +2,16 @@
 
 set -e
 
+if [ "$#" -ne 2 ]; then
+    echo "USAGE: run.sh <input directory> <output directory>"
+    exit 1
+fi
+
 javac -classpath `yarn classpath` -d . WordCountMap.java
 javac -classpath `yarn classpath` -d . WordCountReduce.java
 javac -classpath `yarn classpath`:. -d . WordCountMR.java
 
 jar -cvf wordCount.jar *.class
 
-hadoop jar wordCount.jar WordCountMR /user/rm3485/groupedData output
+hadoop jar wordCount.jar WordCountMR $1 $2
 
